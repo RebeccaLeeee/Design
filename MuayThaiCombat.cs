f2a075085a5f8bbd2ccc592d574df052231a5ee5
RebeccaLee <137526232+RebeccaLeeee@users.noreply.github.com>
2023-09-17 02:40:28 +0200

    Create MuayThaiCombat.cs

---
+++ MuayThaiCombat.cs
@@ -0,0 +1,18 @@
+
+namespace TheGym
+{
+    public class MuayThaiCombat
+    {
+        public bool CanStart() => true;
+        public bool CanWorkout() => true;
+        public bool CanPunch() => true;
+        public bool CanKick() => true;
+        public bool CanUppercut() => true;
+        public bool CanElbow() => true;
+        public bool CanKneel() => true;
+        public bool CanClinch() => true;
+        public bool CanWaiKru() => true;
+        public bool CanShadowBox() => true;
+        public bool CanSparring() => true;
+    }
+}
